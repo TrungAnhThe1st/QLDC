097cd12bbfd59aaf0311f963b175f7fddbad6101
TrungAnhThe1st <geedme728@gmail.com>
2022-05-07 09:33:52 +0700

    Change create-citizen api

--- api/create-citizen.php
+++ api/create-citizen.php
@@ -2,8 +2,16 @@
 if($_SERVER['REQUEST_METHOD'] === "POST"){
     // Sample array
     $qrData = $_POST['qrData'];
+
+    /*0 - new cid, 1 - old cid, 2 - name, 
+    3 - dob (ddMMyyyy), 4 - gender, 5 - address, 
+    6 - cid provided date
+    */
+    $decoded_datas = implode("|", $qrData);
+
     $email = $_POST['email'];
 
+    
     header("Content-Type: application/json");
 
     http_response_code(200);
