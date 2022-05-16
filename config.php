8bcfcd8f1c728010035a9d35b8f012a3daafc663
cuongnew87 <cuongnew37@gmail.com>
2022-05-16 20:09:37 +0700

    commit

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 define('_AMSCODESECURITY', '16343942');
 define('CURRENCY', '$');
-define('WEB_URL', 'http://localhost:8081/Private/ams/');
-define('ROOT_PATH', 'C:\xampp\htdocs\Private\ams/');
+define('WEB_URL', 'https://site.test/QLDC/');
+define('ROOT_PATH', 'D:\xampp\htdocs\QLDC/');
 
 
-define('DB_HOSTNAME', 'localhost:3308');
+define('DB_HOSTNAME', 'localhost:3306');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'ams');
+define('DB_DATABASE', 'demo1_ams');
 $link = new mysqli(DB_HOSTNAME,DB_USERNAME,DB_PASSWORD,DB_DATABASE);?>
