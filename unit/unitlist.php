7a557e7bcddec35e6e3506dfaf4bc71fd65877f9
cuongnew87 <cuongnew37@gmail.com>
2022-05-25 02:14:37 +0700

    add export exel

--- unit/unitlist.php
+++ unit/unitlist.php
@@ -69,13 +69,16 @@ if (isset($_GET['m']) && $_GET['m'] == 'up') {
         </div>
         <!-- /.box-header -->
         <div class="box-body">
-          <table class="table sakotable table-bordered table-striped dt-responsive">
+          <div style="margin:10px">
+            <button id="btnExport" class="btn btn-success" onclick="ExportToExcel('xlsx')">Xuất file exel</button>
+          </div>
+          <table id="testExportId" class="table sakotable table-bordered table-striped dt-responsive">
             <thead>
               <tr>
                 <th><?php echo $_data['floor_no']; ?></th>
                 <th><?php echo $_data['unit_no']; ?></th>
                 <th><?php echo $_data['rent_pm']; ?></th>
-                <th><?php echo $_data['action_text']; ?></th>
+                <th class="excludeExport"><?php echo $_data['action_text']; ?></th>
               </tr>
             </thead>
             <tbody>
@@ -95,7 +98,7 @@ if (isset($_GET['m']) && $_GET['m'] == 'up') {
                   <td><?php echo $row['floor_no']; ?></td>
                   <td><?php echo $row['unit_no']; ?></td>
                   <td><?php echo $ams_helper->currency($localization, $row['rent_pm']); ?></td>
-                  <td>
+                  <td class="excludeExport">
                     <a class="btn btn-success ams_btn_special" data-toggle="tooltip" href="javascript:;" onClick="$('#nurse_view_<?php echo $row['uid']; ?>').modal('show');" data-original-title="QR code"><i class="fa fa-eye"></i></a>
                     <a class="btn btn-warning ams_btn_special" data-toggle="tooltip" href="<?php echo WEB_URL; ?>unit/addunit.php?id=<?php echo $row['uid']; ?>" data-original-title="<?php echo $_data['edit_text']; ?>"><i class="fa fa-pencil"></i></a>
                     <a class="btn btn-danger ams_btn_special" data-toggle="tooltip" onclick="deleteUnit(<?php echo $row['uid']; ?>);" href="javascript:;" data-original-title="<?php echo $_data['delete_text']; ?>"><i class="fa fa-trash-o"></i></a>
@@ -117,7 +120,7 @@ if (isset($_GET['m']) && $_GET['m'] == 'up') {
                     <!--Qr code generating -->
                     <script>
                       var qrcode = new QRCode(document.getElementById("id-qrcode-<?php echo $row['uid']; ?>"), {
-                        text: "<?php echo $row['uid'] . '|' . $row['fid'] . '|' . $row['branch_id'] . '|' . $row['unit_no'] . '|' . $row['floor_no'] . '|' . $row['branch_name'] ; ?>",
+                        text: "<?php echo $row['uid'] . '|' . $row['fid'] . '|' . $row['branch_id'] . '|' . $row['unit_no'] . '|' . $row['floor_no'] . '|' . $row['branch_name']; ?>",
                         width: 200,
                         height: 200,
                         colorDark: "#000000",
@@ -158,3 +161,98 @@ if (isset($_GET['m']) && $_GET['m'] == 'up') {
     });
   </script>
   <?php include('../footer.php'); ?>
+
+  <script>
+    function getIEVersion()
+    // Returns the version of Windows Internet Explorer or a -1
+    // (indicating the use of another browser).
+    {
+      var rv = -1; // Return value assumes failure.
+      if (navigator.appName == 'Microsoft Internet Explorer') {
+        var ua = navigator.userAgent;
+        var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+        if (re.exec(ua) != null)
+          rv = parseFloat(RegExp.$1);
+      }
+      return rv;
+    }
+
+    function tableToExcel(table, sheetName, fileName) {
+
+      var ua = window.navigator.userAgent;
+      var msie = ua.indexOf("MSIE ");
+      if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) // If Internet Explorer
+      {
+        return fnExcelReport(table, fileName);
+      }
+
+      var uri = 'data:application/vnd.ms-excel;base64,',
+        templateData = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body><table>{table}</table></body></html>',
+        base64Conversion = function(s) {
+          return window.btoa(unescape(encodeURIComponent(s)))
+        },
+        formatExcelData = function(s, c) {
+          return s.replace(/{(\w+)}/g, function(m, p) {
+            return c[p];
+          })
+        }
+
+      $("tbody > tr[data-level='0']").show();
+
+      if (!table.nodeType)
+        table = document.getElementById(table)
+
+      var ctx = {
+        worksheet: sheetName || 'Worksheet',
+        table: table.innerHTML
+      }
+
+      var element = document.createElement('a');
+      element.setAttribute('href', 'data:application/vnd.ms-excel;base64,' +
+        base64Conversion(formatExcelData(templateData, ctx)));
+      element.setAttribute('download', fileName);
+      element.style.display = 'none';
+      document.body.appendChild(element);
+      element.click();
+      document.body.removeChild(element);
+
+      $("tbody > tr[data-level='0']").hide();
+    }
+
+    function fnExcelReport(table, fileName) {
+
+      var tab_text = "<table border='2px'>";
+      var textRange;
+
+      if (!table.nodeType)
+        table = document.getElementById(table)
+
+      $("tbody > tr[data-level='0']").show();
+      tab_text = tab_text + table.innerHTML;
+
+      tab_text = tab_text + "</table>";
+      tab_text = tab_text.replace(/<A[^>]*>|<\/A>/g, ""); //remove if u want links in your table
+      tab_text = tab_text.replace(/<img[^>]*>/gi, ""); // remove if u want images in your table
+      tab_text = tab_text.replace(/<input[^>]*>|<\/input>/gi, ""); // reomves input params
+
+      txtArea1.document.open("txt/html", "replace");
+      txtArea1.document.write(tab_text);
+      txtArea1.document.close();
+      txtArea1.focus();
+      sa = txtArea1.document.execCommand("SaveAs", false, fileName + ".xls");
+      $("tbody > tr[data-level='0']").hide();
+      return (sa);
+    }
+
+    $(document).ready(function() {
+      $("#btnExport").click(function() {
+        var tempTable = $("#testExportId").html();
+
+        $("#testExportId .excludeExport").remove();
+
+        tableToExcel("testExportId", "test", "trys");
+
+        $("#testExportId").html(tempTable);
+      });
+    });
+  </script>
